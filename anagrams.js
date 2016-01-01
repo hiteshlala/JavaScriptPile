e11d48032cbcfea8f2e6cafae1827abb2c1ca537
hiteshlala <hash_lala@yahoo.com>
2016-01-01 10:46:45 -0800

    adds second anagram function

--- anagrams.js
+++ anagrams.js
@@ -24,3 +24,18 @@ function anagram(string) {
   
   return result;
 }
+
+function anagram2(string, result, anag) {
+  result = result || [];
+  anag = anag || "";
+  if(string.length === 0) {
+    result.push(anag);
+  } else {
+    for(var i = 0; i < string.length; i++) {
+      var temp = string.slice(0,i) + string.slice(i+1);
+      anagram2(temp, result, anag + string[i]);
+    }
+  }
+  return result;
+}
+
